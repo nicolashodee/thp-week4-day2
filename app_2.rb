0f76fe49d7a600200c1e59fd6ab128f31607153a
Nicolas HODEE <accounts@nicolashodee.com>
2020-04-21 16:35:03 +0100

    second version finished

---
+++ app_2.rb
@@ -0,0 +1,74 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "------------------------------------------------"
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant !|"
+puts "-------------------------------------------------"
+
+puts "Donne nous ton nom mon jeune enfant :"
+name_to_save = gets.chomp.to_s 
+human_player = HumanPlayer.new("#{name_to_save}")
+
+puts "Ton prenom est donc #{human_player.name}, tu as #{human_player.life_points} points de vie, et une arme de niveau #{human_player.weapon_level}"
+player1 = Player.new("Pierre")
+player2 = Player.new("Paul")
+puts "------------------------------------------------"
+while (human_player.life_points > 0 && ( player1.life_points > 0 && player2.life_points > 0 ))
+  puts "------------ETAT---DES---LIEUX------------------"
+  human_player.show_state
+  player1.show_state
+  player2.show_state
+  print "Pret pour faire un choix ? (appuie sur entree)"
+  gets.chomp
+  puts "------------FAIS---TON---CHOIX--------------------"
+  puts "a - chercher une meilleure arme
+  s - chercher à se soigner 
+  "
+  puts " attaquer un joueur en vue :
+  0 - #{player1.name} 
+  1 - #{player2.name}
+  ------------------------------------------------"
+  print "Ton choix est > "
+  choice = gets.chomp
+  unless (choice == "a" || choice == "s" || choice == "0" || choice == "1")
+    puts "Erreur, retape ton choix :"
+    choice = gets.chomp 
+  end
+  
+  if choice == "a"
+    human_player.search_weapon 
+  elsif choice == "s"
+    human_player.search_healthpack 
+  elsif choice == "0"
+    human_player.attacks(player1)
+  elsif choice == "1"
+    human_player.attacks(player2)
+  else
+  end     
+  
+  
+  puts "Maintenant les autres joueurs t'attaquent. Pret(e) ? "
+  gets.chomp 
+  ennemies = [player1, player2]
+  ennemies.each { |ennemy| 
+      if human_player.life_points > 0 && ennemy.life_points > 0
+        ennemy.attacks(human_player)
+        puts "***************"
+      else 
+        puts "#{ennemy.name} abandonne l'attaque."
+        puts "***************" 
+      end
+      puts "Pret pour la suite ? (appuie sur entree si oui)"
+      gets.chomp 
+    }
+end
+puts "------------GAME---OVER--------------------"
+human_player.show_state
+ennemies.each { |ennemy| 
+  ennemy.show_state }
+puts "-------------------------------------------"
+
--- lib/player.rb
+++ lib/player.rb
@@ -25,11 +25,64 @@ class Player
 
   def attacks(player_b)
     attack_value = compute_damage
+    puts "bouyaka !!!"
     puts "#{self.name} attaque #{player_b.name}"
     puts "Il/elle lui inflige #{attack_value} points de dommages"
     player_b.gets_damage(attack_value)
     puts player_b.show_state
   end 
-  
-  
+    
 end 
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level 
+  
+  def initialize(name_to_save) 
+    @weapon_level = 1
+    super (name_to_save)
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end 
+
+  def search_weapon
+    dice = rand(1..6)
+    if dice == 1
+      puts "C'est balo, t'as rien trouvé."
+    else
+      if dice > @weapon_level
+        puts "Tu as trouvé une arme de niveau #{dice}, qui est meilleure arme que la tienne. Tu la prends !"
+        @weapon_level = dice 
+      else
+        puts "M@*#$, tu as seulement trouvé une arme de niveau #{dice}, qui est moins bien ou equivalente a la tienne. Tu la laisse. "
+      end 
+    end 
+  end
+
+  def search_healthpack 
+    dice = rand(1..6)
+    if dice == 1
+      puts "C'est balo, t'as rien trouvé."
+    else
+      if dice >= 2 && dice <=5
+        puts "Tu as trouvé un trousse de 50 points de vie, Tu la prends !"
+        puts "Tu avais #{life_points} points de vie."
+        @life_points += 50 
+        @life_points.clamp(0, 100)
+        puts "Tu en as maintenant #{life_points}."   
+      else
+        puts "Tu as trouvé un trousse de 80 points de vie, Tu la prends !"
+        puts "Tu avais #{life_points} points de vie."
+        @life_points += 80 
+        @life_points.clamp(0, 100)
+        puts "Tu en as maintenant #{life_points}."  
+      end
+    end 
+  end 
+
+
+
+end 
+
+
